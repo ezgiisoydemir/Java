2e3d9613b1789ad5ed0c82813708e5117c7da31c
Ezgi Soydemir <41003405+ezgiisoydemir@users.noreply.github.com>
2022-07-22 18:36:48 +0300

    Add files via upload

---
+++ Day16/Task_NestedLoop2.java
@@ -0,0 +1,29 @@
+package Day16;
+
+public class Task_NestedLoop2 {
+
+    // print following pattern
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+    // *****
+
+
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 6; i++) {
+
+            for (int j = 0; j < 5; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+
+        }
+
+    }
+
+}
---
+++ Day19/_01_Methods2.java
@@ -0,0 +1,17 @@
+package Day19;
+
+public class _01_Methods2 {
+
+    public static void main(String[] args) {
+
+        printHello5Times();
+
+    }
+
+
+    public static void printHello5Times() {
+        for (int i = 0; i < 5; i++)
+            System.out.println("Hello");
+    }
+
+}
