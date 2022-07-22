242988eb8931cdaddf6258bcdd036b9b987bc549
Ezgi Soydemir <41003405+ezgiisoydemir@users.noreply.github.com>
2022-07-22 18:41:18 +0300

    Add files via upload

---
+++ Day42/CustomExceptions/YouAreWrongBro.java
@@ -0,0 +1,11 @@
+package Day42.CustomExceptions;
+
+public class YouAreWrongBro extends RuntimeException {
+
+    /**
+     * If you want to create checked exception you need to extend "Exception" class
+     *
+     * If you want to create unchecked exception you need to extend "RuntimeException" class
+     */
+
+}
